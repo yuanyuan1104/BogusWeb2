ac07c47c4c0aa7c7e697f98d1fab76ecf960d9f8
Yuanyuan Zhang <yuanyuanzhang1104@gmail.com>
2019-09-18 17:49:55 -0700

    second commit

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace lab.Controllers
 {
     public class FileController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public FileController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
         public IActionResult Index()
         {
-            string[] file = Directory.GetFiles("./TextFiles");
+            // string webRootPath = _hostingEnvironment.WebRootPath;
+            var path = Path.Combine(_hostingEnvironment.WebRootPath, "./TextFiles");
+            string[] file = Directory.GetFiles(path);
+            // string[] filename = Path.GetFileName(file);
             ViewData["files"] = file;
             return View();
         }
--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using lab.Models.NW;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,6 +32,9 @@ namespace lab
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+            services.AddDbContext<NorthwindContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("Northwind"))
+);
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
